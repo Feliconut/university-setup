a6f920752833c72f58735adf102b717e5ed9188f
Xiaoyu Liu <feliconut@outlook.com>
2022-08-10 16:36:16 -0400

    refactor

--- scripts/chooses.py
+++ scripts/chooses.py
@@ -3,28 +3,28 @@
 import choose_courses
 import choose_lectures
 import choose_lectures_view
-from choose import rofi,Rofi
+from choose import rofi, Rofi
 # register all services and options
 services = [choose_courses, choose_lectures, choose_lectures_view]
 
 # generate list of all options, pass to choose
-indices = []
-options = []
-opt_service = []
+# (service, indice, opt)
+data = []
 for service in services:
-    options += service.options
-    indices += list(range(0,len(service.options)))
-    opt_service += [service]*len(service.options)
+    data += list(zip(
+        [service]*len(service.options),
+        list(range(len(service.options))),
+        service.options))
 
-returncode, index, selected = rofi('Select option', options, [ ])
+returncode, index, selected = rofi('Select option', [option for _,_,option in data], [])
 # parse the result, send to service process
 
 if returncode == Rofi.SELECTED:
     try:
-        service = opt_service[index]
+        service, serv_index, selected = data[index]
         service.process(
             returncode,
-            indices[index],
+            serv_index,
             selected
         )
 
